87e8d9327d4730409ae32f3003893730a4d146be
uzinok <uzinok@yandex.ru>
2022-03-05 15:25:58 +0300

    добавляет поле пароля

--- dest/scripts/main.js
+++ dest/scripts/main.js
@@ -1,22 +1,37 @@
 "use strict";
 
-function setIconSelect(elem) {
-  elem.classList.add('form__select-icon');
-  addDataIcon(elem);
-  elem.addEventListener('change', function () {
+// form
+// select
+if (document.querySelectorAll('.form__select-icon--js')) {
+  var setIconSelect = function setIconSelect(elem) {
+    elem.classList.add('form__select-icon');
     addDataIcon(elem);
+    elem.addEventListener('change', function () {
+      addDataIcon(elem);
+    });
+  };
+
+  var addDataIcon = function addDataIcon(elem) {
+    return elem.dataset.icon = elem.querySelector('select').value.toLowerCase();
+  };
+
+  var selectIcon = document.querySelectorAll('.form__select-icon--js');
+  selectIcon.forEach(function (elem) {
+    setIconSelect(elem);
+  });
+} // password
+
+
+if (document.querySelectorAll('.form__password--js')) {
+  var arrInputPassword = document.querySelectorAll('.form__password--js');
+  arrInputPassword.forEach(function (elem) {
+    var button = elem.querySelector('button');
+    var input = elem.querySelector('input');
+    button.removeAttribute('style');
+    button.addEventListener('click', function () {
+      this.classList.toggle('form__password-toggle--visible');
+      if (input.type == 'password') return input.setAttribute('type', 'text');
+      return input.setAttribute('type', 'password');
+    });
   });
 }
-
-function addDataIcon(elem) {
-  return elem.dataset.icon = elem.querySelector('select').value.toLowerCase();
-}
-
-window.addEventListener('load', function () {
-  if (document.querySelectorAll('.form__select-icon--js')) {
-    var selectIcon = document.querySelectorAll('.form__select-icon--js');
-    selectIcon.forEach(function (elem) {
-      setIconSelect(elem);
-    });
-  }
-});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,41 @@
-function setIconSelect(elem) {
-	elem.classList.add('form__select-icon');
-	addDataIcon(elem);
+// form
+// select
+if (document.querySelectorAll('.form__select-icon--js')) {
+	const selectIcon = document.querySelectorAll('.form__select-icon--js');
 
-	elem.addEventListener('change', () => {
+	selectIcon.forEach(elem => {
+		setIconSelect(elem);
+	});
+
+	function setIconSelect(elem) {
+		elem.classList.add('form__select-icon');
 		addDataIcon(elem);
+
+		elem.addEventListener('change', () => {
+			addDataIcon(elem);
+		});
+	}
+
+	function addDataIcon(elem) {
+		return elem.dataset.icon = elem.querySelector('select').value.toLowerCase();
+	}
+}
+
+// password
+if (document.querySelectorAll('.form__password--js')) {
+	const arrInputPassword = document.querySelectorAll('.form__password--js');
+
+	arrInputPassword.forEach(elem => {
+		const button = elem.querySelector('button');
+		const input = elem.querySelector('input');
+
+		button.removeAttribute('style');
+
+		button.addEventListener('click', function () {
+			this.classList.toggle('form__password-toggle--visible')
+			if (input.type == 'password')
+				return input.setAttribute('type', 'text');
+			return input.setAttribute('type', 'password');
+		})
 	});
 }
-
-function addDataIcon(elem) {
-	return elem.dataset.icon = elem.querySelector('select').value.toLowerCase();
-}
-
-window.addEventListener('load', () => {
-	if (document.querySelectorAll('.form__select-icon--js')) {
-		const selectIcon = document.querySelectorAll('.form__select-icon--js');
-		selectIcon.forEach(elem => {
-			setIconSelect(elem);
-		})
-	}
-})
