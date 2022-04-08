d168c1135c34842c383f4787206b25306f20a4e7
uzinok <41639106+uzinok@users.noreply.github.com>
2022-04-08 16:41:25 +0300

    добавляет скрипт для details

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -98,7 +98,6 @@ if (document.querySelector('.form__number-wrap--js')) {
 		});
 
 		minus.addEventListener('click', () => {
-			console.log(!input.value);
 			if (!input.value)
 				return input.value = input.min;
 			const value = +input.value - +input.getAttribute('step');
@@ -116,9 +115,7 @@ if (document.querySelector('.form__social--js')) {
 	const wrap = document.querySelector('.form__social--js');
 	const select = wrap.querySelector('select');
 	const allInput = wrap.querySelectorAll('input');
-	// console.log(allInput.length);
 	for (let i = 0; allInput.length > i; i++) {
-		console.log(allInput);
 		allInput[i].setAttribute('tabindex', -1);
 	}
 
@@ -134,4 +131,105 @@ if (document.querySelector('.form__social--js')) {
 		activeInput.classList.add('form__social-input--active');
 		activeInput.setAttribute('tabindex', 0);
 	})
+}
+
+// details summary
+
+if (document.querySelector('details')) {
+	// добавим стили для анимации
+	let styleD = document.createElement('style');
+	styleD.innerHTML = '.details_content{overflow:hidden;opacity:0;max-height:0;-webkit-transition:max-height var(--transition),opacity var(--transition);-o-transition:max-height var(--transition),opacity var(--transition);transition:max-height var(--transition),opacity var(--transition)}.details-visible .details_content{overflow:auto;opacity:1;max-height:190px}.details_h .details_content{display:none}details>button{border-radius:none;border:0;-webkit-box-shadow:none;box-shadow:none;background:0 0;font-family:none;font-size:none;padding:0;text-align:inherit}';
+	// добавим стили в head
+	document.head.appendChild(styleD);
+	// ролучаем все спойлеры
+
+	let arrDetails = document.querySelectorAll('details');
+
+	// перебираем все спойлееры для получения необходимых элементов и прослушивания событий
+	for (let i = 0; i < arrDetails.length; i++) {
+
+		// по кнопке summary будем отслеживать клик
+		let elemSummary;
+		// если summary отсутствует - добавляем
+		if (!(arrDetails[i].querySelector('summary'))) {
+			// если нет поддержки details
+			if (!('open' in arrDetails[i]))
+				// создаем кнопку
+				elemSummary = document.createElement('button');
+			else
+				// или содаем summary
+				elemSummary = document.createElement('summary');
+			// добавим текст по умолчанию
+			elemSummary.innerHTML = 'подробнее';
+			// добавляем текущему спойлеру
+			arrDetails[i].insertBefore(elemSummary, arrDetails[i].querySelector('.details_content'))
+		}
+		// если summary есть - получаем
+		else {
+			elemSummary = arrDetails[i].querySelector('summary');
+			// если нет поддержки details
+			if (!('open' in arrDetails[i])) {
+				// тут необходимо вместо summary сделать button
+				let button = document.createElement('button');
+				// перенесли текст
+				button.innerHTML = elemSummary.innerHTML;
+				// перенесли классы
+				button.setAttribute('class', elemSummary.getAttribute('class'));
+				// elemSummary = button;
+				// добавили в спойлер кнопку
+				arrDetails[i].insertBefore(button, elemSummary);
+				// удалили старый summary
+				arrDetails[i].removeChild(elemSummary);
+
+				elemSummary = button;
+			}
+		}
+
+		// если спойлер открыт
+		if (arrDetails[i].hasAttribute('open')) {
+			// добавляем класс спойлеру что бы контент был виден
+			arrDetails[i].classList.add('details-visible');
+		}
+		// для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
+		else if (!('open' in arrDetails[i])) {
+			arrDetails[i].classList.add('details_h');
+		}
+
+		// отслеживаем клик
+		elemSummary.addEventListener('click', function (event) {
+			console.dir(event.target.nodeName == 'INPUT');
+
+			if (event.target.nodeName == 'INPUT') return;
+
+			// получаем родительский блок
+			let parentBlock = this.parentNode;
+
+			// если открыт спойлер
+			if (parentBlock.classList.contains('details-visible')) {
+				// отменяем действие по умолчанию для браузеров поддерживоющих <details>
+				event.preventDefault();
+				// с анимацией скрываем контент
+				parentBlock.classList.remove('details-visible');
+
+				// после завершения анимации
+				setTimeout(function() {
+					if (!('open' in parentBlock))
+						// для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
+						parentBlock.classList.add('details_h');
+					else
+						// для новых - удаляем атрибут open
+						parentBlock.open = false;
+				}, 200)
+			}
+			// если спойлер закрыт
+			else {
+				// для старых браузеров удаляем display none для контента
+				parentBlock.classList.remove('details_h');
+				// через минимальный промежуток времени запускаем анимацию css появления контента
+				setTimeout(function() {
+					parentBlock.classList.add('details-visible');
+				}, 5)
+			}
+		})
+	}
 }
